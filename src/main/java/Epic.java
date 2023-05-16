0df9ac8b300b08ddf2744c1461d5d53ad86082f8
AndreyPlatonov <one.and.pl@gmail.com>
2023-05-17 00:17:33 +0300

    class test

---
+++ src/main/java/Epic.java
@@ -0,0 +1,9 @@
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id); // вызов родительского конструктора
+        this.subtasks = subtasks; // заполнение своих полей
+    }
+
+}
